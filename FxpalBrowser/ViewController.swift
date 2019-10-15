7ac7632d736157c373810064fbc952650687fc10
Yulius Tjahjadi <yultide@yahoo.com>
2019-10-14 21:37:12 -0700

    Initial Commit

---
+++ FxpalBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FxpalBrowser
+//
+//  Created by Yulius Tjahjadi on 10/14/19.
+//  Copyright © 2019 White Tea LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
